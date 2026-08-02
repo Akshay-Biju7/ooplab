f58c1c9c9f782bafa31af9954dea4d40a1d3c28b
Akshay-Biju7 <akshaybiju940@gmail.com>
2026-08-02 22:44:01 +0530

    Add files via upload

---
+++ PrimeNumbers1To100.java
@@ -0,0 +1,19 @@
+package oopc;
+public class PrimeNumbers1To100 {
+    public static void main(String[] args) {
+
+        for (int num = 2; num <= 100; num++) {
+            boolean prime = true;
+
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime)
+                System.out.print(num + " ");
+        }
+    }
+}
